f2dfc08bf256fd9ed4bcc5b6d07f1189c951ff2b
simo-02 <milanov.simeon@yahoo.com>
2023-06-02 15:21:55 +0200

    Initial commit

---
+++ src/test/java/com/example/Habit/Tracker/Api/HabitTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Habit.Tracker.Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
