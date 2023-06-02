1d6a497ccc1946cb88fbf2d87af40c0652cbe1e7
simo-02 <milanov.simeon@yahoo.com>
2023-06-02 17:33:58 +0200

    Update

--- src/main/java/com/example/Habit/Tracker/Api/HabitTrackerApiApplication.java
+++ src/main/java/com/example/Habit/Tracker/Api/HabitTrackerApiApplication.java
@@ -1,11 +1,7 @@
 package com.example.Habit.Tracker.Api;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-
-import java.util.List;
 
 @SpringBootApplication
 public class HabitTrackerApiApplication {
