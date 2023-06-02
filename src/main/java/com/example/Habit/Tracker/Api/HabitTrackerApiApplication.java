ff542bfcfe9ade71f69e2a5688e8dfcf3dce567a
mertm27 <mert.miftar7@gmail.com>
2023-06-02 15:53:06 +0200

    Update commit

--- src/main/java/com/example/Habit/Tracker/Api/HabitTrackerApiApplication.java
+++ src/main/java/com/example/Habit/Tracker/Api/HabitTrackerApiApplication.java
@@ -3,8 +3,10 @@ package com.example.Habit.Tracker.Api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HabitTrackerApiApplication {
 
 	public static void main(String[] args) {
